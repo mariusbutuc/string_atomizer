bf06d5f70cf414f01d2255a1a7eb775db4c10a2d
Mike Stok <mike@stok.ca>
2017-03-22 09:14:32 -0400

    add code for StringAtomizer.from_camel_case
    Copied regex from stack overflow. This may be too clever, but it works!

--- lib/string_atomizer.ex
+++ lib/string_atomizer.ex
@@ -4,15 +4,33 @@ defmodule StringAtomizer do
   """
 
   @doc """
-  Hello world.
+  Take a CamelCase string and turn it into an unserscored atom.
 
-  ## Examples
+      iex> StringAtomizer.from_camel_case("MyTestKey")
+      :my_test_key
 
-      iex> StringAtomizer.hello
-      :world
+      iex> StringAtomizer.from_camel_case("myTestKey")
+      :my_test_key
 
+      iex> StringAtomizer.from_camel_case("AccountID")
+      :account_id
+
+      iex> StringAtomizer.from_camel_case("AccountIDForUser")
+      :account_id_for_user
+
+      iex> StringAtomizer.from_camel_case("UserKey123")
+      :user_key123
+
+      iex> StringAtomizer.from_camel_case("123FourFive6")
+      :"123_four_five6"
+
+  Regular expression lifted from http://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-snake-case
   """
-  def hello do
-    :world
+  @spec from_camel_case(String.t) :: atom
+  def from_camel_case(string) do
+    ~r/((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))/
+            |> Regex.replace(string, "_\\1")
+            |> String.downcase
+            |> String.to_atom
   end
 end
